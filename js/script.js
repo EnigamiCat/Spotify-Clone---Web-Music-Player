d1b8002d20b2d8255c1450558b281e16bf17b0b7
Govind Kumar <govindkumar26112001@gmail.com>
2024-07-16 02:44:22 +0530

    sixth commit

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ let currentSong = new Audio;
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     const response = await a.text()
 
 
@@ -82,7 +82,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums(){
-      let a = await fetch(`http://127.0.0.1:3000/songs/`)
+      let a = await fetch(`/songs/`)
       let response = await a.text();
       let div = document.createElement("div")
       div.innerHTML = response;
@@ -98,10 +98,10 @@ async function displayAlbums(){
       
 
       
-        if(e.href.includes("/songs")){
+        if(e.href.includes("/songs") && !e.href.includes(".htaccess")){
             let folder = e.href.split("/").slice(-2)[0]
             //get metadata of the folder
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `  <div data-folder="${folder}" class="card">
@@ -194,6 +194,9 @@ async function main() {
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change",(e)=>{
         console.log("setting volume as ",e.target.value," / 100")
         currentSong.volume = parseInt(e.target.value)/100
+        if(currentSong.volume>0){
+            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg","volume.svg")
+        }
     })
 
    
